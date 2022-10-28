ab60b037481143714cef5a1e7d063c2744465afd
Sagar Sarkale <sagxam@gmail.com>
2022-10-28 13:46:35 +0530

    [add] logger fixes

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import logging
 
 
@@ -7,7 +8,7 @@ class Logger:
         """
         https://docs.python.org/3/howto/logging.html#loggers
         https://www.loggly.com/ultimate-guide/python-logging-basics/
-        https://stackoverflow.com/questions/6729268/log-messages-appearing-twice-with-python-logging
+        https://github.com/IDSIA/sacred/issues/268#issuecomment-459748353
 
         level       numeric_value
         CRITICAL    50
@@ -22,11 +23,13 @@ class Logger:
         loglevel = os.environ.get("LOG_LEVEL", "INFO")
 
         # create logger
-        logger = logging.getLogger("util_logger")
-        logger.setLevel(loglevel)
+        self.logger = logging.getLogger("util_logger")
+        self.logger.propagate = False
+        self.logger.handlers = []
+        self.logger.setLevel(loglevel)
 
         # create console handler and set level to debug
-        ch = logging.StreamHandler()
+        ch = logging.StreamHandler(sys.stderr)
         ch.setLevel(loglevel)
 
         # create formatter
@@ -36,13 +39,7 @@ class Logger:
         ch.setFormatter(formatter)
 
         # add ch to logger
-        logger.addHandler(ch)
-
-        logger.propagate = False
-
-        self.logger = logger
-
-        # print(loglevel, self.logger)
+        self.logger.addHandler(ch)
 
     def d(self, msg):
         self.logger.debug(msg)
@@ -60,9 +57,9 @@ class Logger:
         self.logger.critical(msg)
 
 
-if __name__ == "__main__":
-    log = Logger()
+# if __name__ == "__main__":
+#     log = Logger()
 
-    log.d("hello debug")
-    log.i("hello info")
-    log.e("hello error")
+#     log.d("hello debug")
+#     log.i("hello info")
+#     log.e("hello error")
