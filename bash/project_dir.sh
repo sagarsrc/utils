c2fc72c8c41be01c617a50184e47d750041215fc
Sagar Sarkale <31827531+sagarsrc@users.noreply.github.com>
2024-05-11 20:05:30 +0530

    Update project_dir.sh

--- bash/project_dir.sh
+++ bash/project_dir.sh
@@ -2,7 +2,7 @@ mkdir -p src/utils src/config
 mkdir -p data/raw data/clean
 mkdir -p notebooks
 
-touch src/main.py src/config/config.py
+touch src/main.py
 touch .env
 
-wget https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore
+wget https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore -O .gitignore
