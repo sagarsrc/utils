16b22666114fc987eea1fb0a017a84c6331b1b3e
Sagar Sarkale <31827531+sagarsrc@users.noreply.github.com>
2024-07-22 11:22:56 +0530

    Update colab.sh

--- bash/colab.sh
+++ bash/colab.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 # Install libraries if not already present
-for lib in "xformers<0.0.27" "trl<0.9.0" peft accelerate bitsandbytes wandb cutility pandarallel; do
-    if ! python -c "import ${lib%%<*}" 2>/dev/null; then
+libs=("xformers<0.0.27" "trl<0.9.0" "peft" "accelerate" "bitsandbytes" "wandb" "cutility" "pandarallel")
+
+for lib in "${libs[@]}"; do
+    libname=${lib%%<*}  # Remove version constraint if present
+    if ! python -c "import $libname" 2>/dev/null; then
         pip install -q "$lib"
     else
-        echo "${lib%%<*} is already installed. Skipping installation."
+        echo "$libname is already installed. Skipping installation."
     fi
 done
 
 # Print library versions
-python -c "
+python << END
 import transformers
 import accelerate
 import peft
 print('---')
 print(f'Transformers version: {transformers.__version__}')
 print(f'Accelerate version: {accelerate.__version__}')
-print(f'PEFT version: {peft.__version__}')"
+print(f'PEFT version: {peft.__version__}')
+END
